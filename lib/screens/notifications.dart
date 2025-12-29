b76fc281dfcd14aad7f6284682188fcc977f11b6
SanManjiro <karmelkeke@gmail.com>
2025-12-29 20:21:14 +0100

    Ajou de le fonctionnalite de suppresion d'une notification au balayage

--- lib/screens/notifications.dart
+++ lib/screens/notifications.dart
@@ -1,51 +1,88 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_class_project/models/notification.dart';
 
-class NotificationPage extends StatelessWidget {
+class NotificationPage extends StatefulWidget {
   final List<NotificationModel> notif;
   const NotificationPage({super.key, required this.notif});
+
+  @override
+  State<NotificationPage> createState() => _NotificationPageState();
+}
+
+class _NotificationPageState extends State<NotificationPage> {
+  late List<NotificationModel> _notifications;
+
+  @override
+  void initState() {
+    super.initState();
+    _notifications = List<NotificationModel>.from(widget.notif);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Notifications')),
-      body: notif.isEmpty
+      body: _notifications.isEmpty
           ? const Center(child: Text('Aucune notification'))
-          // ListView.builder permet ici de creer une notification a la demande  
+          // ListView.builder permet de créer les éléments à la demande dans ce cas des notifications 
           : ListView.builder(
-              itemCount: notif.length,
+              itemCount: _notifications.length,
               itemBuilder: (context, index) {
-                final n = notif[index];
-                //Ici le LIstille est le composant qui represente une ligne des notification
-                return ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: n.isRead
-                        ? Colors.grey.shade300
-                        : Colors.green,
-                    child: Icon(
-                      n.isRead
-                          ? Icons.notifications_none
-                          : Icons.notification_important,
-                      color: Colors.white,
+                final n = _notifications[index];
+                // Dismissible est un widget Flutter qui permet de supprime un élément par balayage.On fournit une cle unique et on gère la suppression dans onDismissed.
+                return Dismissible(
+                  key: ValueKey(n.id),
+                  direction: DismissDirection.endToStart,
+                  background: Container(
+                    color: Colors.red,
+                    alignment: Alignment.centerRight,
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: const Icon(Icons.delete, color: Colors.white),
+                  ),
+                  //Action a la suppresion d'une notification
+                  onDismissed: (_) {
+                    // On supprime l'element localement et on met à jour l'interface
+                    setState(() {
+                      _notifications.removeAt(index);
+                    });
+                    
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Notification supprimée')),
+                    );
+                  },
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: n.isRead
+                          ? Colors.grey.shade300
+                          : Colors.green,
+                      child: Icon(
+                        n.isRead
+                            ? Icons.notifications_none
+                            : Icons.notification_important,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                  title: Text(
-                    n.title,
-                    style: TextStyle(
-                      fontWeight: n.isRead
-                          ? FontWeight.normal
-                          : FontWeight.bold,
+                    title: Text(
+                      n.title,
+                      style: TextStyle(
+                        fontWeight: n.isRead
+                            ? FontWeight.normal
+                            : FontWeight.bold,
+                      ),
                     ),
+                    subtitle: Text(
+                      n.message,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    trailing: Text(
+                      n.timestamp,
+                      style: const TextStyle(fontSize: 12, color: Colors.grey),
+                    ),
+                    onTap: () {
+                      
+                    },
                   ),
-                  subtitle: Text(
-                    n.message,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  trailing: Text(
-                    n.timestamp,
-                    style: const TextStyle(fontSize: 12, color: Colors.grey),
-                  ),
-                  onTap: () {},
                 );
               },
             ),
