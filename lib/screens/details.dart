b016091e0f8d064ed2586d1c7ea46c5211afb7cd
Abel <lewheabel16@gmail.com>
2025-12-29 11:29:29 +0100

    progression bar

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -13,25 +13,51 @@ class DetailsPage extends StatelessWidget {
         child: Column(
           children: [
             Card(
-              clipBehavior: Clip.antiAlias, 
+              clipBehavior: Clip.antiAlias,
               shape: RoundedRectangleBorder(
                 // side: BorderSide(width: 2.0),
-                borderRadius: BorderRadius.circular(12.0)
+                borderRadius: BorderRadius.circular(12.0),
               ),
 
               margin: EdgeInsets.all(10),
-              child: Image.network(feed.imageUrl, width: double.infinity, height: 300, fit: BoxFit.cover,),
+              child: Image.network(
+                feed.imageUrl,
+                width: double.infinity,
+                height: 300,
+                fit: BoxFit.cover,
+              ),
             ),
             Padding(
               padding: EdgeInsetsGeometry.all(10),
               child: Text(
-              feed.title,
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ) ,)
-            
+                feed.title,
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+            // Divider(height: 30),
+            // 3. Barre de progression (Données de ton modèle)
+            // Text("Objectif : ${feed.target}\$"),
+            SizedBox(height: 8),
+            Row(
+              children: [
+                Text("Raised : \$ ${feed.raised}"),
+                Text("Target : \$ ${feed.target}"),
+              ],
+            ),
+            LinearProgressIndicator(
+              value: feed.raised / feed.target, // Ratio de progression
+              backgroundColor: Colors.grey[200],
+              color: Colors.green,
+            ),
+            Row(
+              children: [
+                Text("${(feed.raised / feed.target) * 100} % target reached",style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold),),
+                Text("${feed.timeElapsed} left")
+              ],
+            )
           ],
         ),
-      )
+      ),
     );
   }
 }
