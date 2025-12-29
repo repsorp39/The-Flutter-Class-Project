dcb8ad6f8f2a38b9ab8028ea04629c6ddfe66c89
Abel <lewheabel16@gmail.com>
2025-12-29 11:14:00 +0100

    show feed image in card

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -9,7 +9,29 @@ class DetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Page details')),
-      body: Text("Le composant detail sur cette page"),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Card(
+              clipBehavior: Clip.antiAlias, 
+              shape: RoundedRectangleBorder(
+                // side: BorderSide(width: 2.0),
+                borderRadius: BorderRadius.circular(12.0)
+              ),
+
+              margin: EdgeInsets.all(10),
+              child: Image.network(feed.imageUrl, width: double.infinity, height: 300, fit: BoxFit.cover,),
+            ),
+            Padding(
+              padding: EdgeInsetsGeometry.all(10),
+              child: Text(
+              feed.title,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ) ,)
+            
+          ],
+        ),
+      )
     );
   }
 }
